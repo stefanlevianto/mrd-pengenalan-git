307c1900713d8c7cddbafb1070c11293accb65e1
stefanlevianto <stefanlevianto@gmail.com>
2022-05-24 19:48:41 +0700

    feat: ini adalah commitan pertamaku

---
+++ index.js
@@ -0,0 +1,8 @@
+//membuat const pi
+const pi = 3.14;
+console.log("hallo dunia,ini saya stefan!");
+console.log(pi);
+console.log()
+let y = 3; y--; console.log(y * 0.5)
+let x = 2; x = "aku"; console.log(x + 4);
+const a = 4; a++; console.log(a - 3);
